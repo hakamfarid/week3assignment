5fcdbe22c6b7575efd9c78e7a35f2b6297a293f4
hakamfarid <hakamfarid@gmail.com>
2020-11-16 23:24:10 +0000

    New submission for week 3 assignment

--- matrix2.R
+++ matrix2.R
@@ -1,3 +1,4 @@
+##function to cache the matrix as well as the inverse
 makeCacheMatrix <- function(x=matrix()){
   inv <- NULL
   set <- function(y){
@@ -10,11 +11,11 @@ makeCacheMatrix <- function(x=matrix()){
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
   
 }
-
+##function to solve for the inverse of a matrix 
 cachesolve <- function(x, ...){
   inv<- x$getinverse()
   if(!is.null(inv)){
-    message("getting chache data...")
+    message("getting chache data...")       
     return(inv)
   }
   mat <- x$get()
